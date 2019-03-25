050dc2ecbba8011efba7b3b239f94b3bd015e250
Kubr <kubr2017@gmail.com>
2019-03-25 15:12:32 -0400

    Feat: Add menu image to folder 'public'. Add HTML code to <div> element for Hours Adjusted design Open Hours and DAys.

--- src/components/Place.js
+++ src/components/Place.js
@@ -10,16 +10,21 @@ class Place extends React.Component {
     console.log('inside place, name:',this.props.place.name,' LoadStatus:',this.props.LoadStatus,' focus:',this.props.focus,' inside place rate:',this.props.place.rate);
     let hoursStr = 'No hours'
     if (!(this.props.place.hours==='No hours')){
-      hoursStr='days:'+this.props.place.hours.days+' open hours:'+this.props.place.hours.open[0].renderedTime
+      hoursStr='<h3>Days:</h3> '+this.props.place.hours.days+'<h3>Open hours:</h3><p>'+this.props.place.hours.open[0].renderedTime+'</p>'
       console.log('inside place condition No hours name:'+this.props.place.name+'hoursStr:'+hoursStr);
       //<iframe id="frame" src={this.props.place.menu} width='300' height='300' scrolling="yes"></iframe>
     }
     return(
       <div onClick={this.getFocus}><h2 className='placeName'>{this.props.place.name}</h2><hr/>
       <p className='placeAddress'>{this.props.place.address}</p>
-       <img src={this.props.place.pic} alt={this.props.place.pic}></img>
-        {hoursStr}
-      <p>Rate: {this.props.place.rate}</p><a href={this.props.place.menu==='No menu' ? null:this.props.place.menu} target='_blank'>{this.props.place.menu==='No menu' ? 'No menu':'Menu'}</a>
+      <div className='placeDetailsContainer'>
+        <div className='placeTimeDeatils'>
+          <div dangerouslySetInnerHTML={{ __html: hoursStr }} />
+        </div>
+        <img src={this.props.place.pic} alt={this.props.place.pic}></img>
+        <a href={this.props.place.menu==='No menu' ? null:this.props.place.menu} target='_blank'>{this.props.place.menu==='No menu' ? 'No menu':'Menu'}</a>
+      </div>
+      <p>Rate: {this.props.place.rate}</p>
       </div>
     )
   }
