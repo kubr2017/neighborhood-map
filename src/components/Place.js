bd416a9ab46b1f36d4db7c45e048f9e88fcbf803
Kubr <kubr2017@gmail.com>
2019-03-26 12:16:21 -0400

    Chore: Mediocr design

--- src/components/Place.js
+++ src/components/Place.js
@@ -15,12 +15,14 @@ class Place extends React.Component {
       //<iframe id="frame" src={this.props.place.menu} width='300' height='300' scrolling="yes"></iframe>
     }
     return(
-      <div onClick={this.getFocus}><h2 className='placeName'>{this.props.place.name}</h2>
+      <div onClick={this.getFocus}>
+      <h2 className='placeName'>{this.props.place.name}</h2>
       <h3 className='placeAddress'>{this.props.place.address}</h3>
+      <span className='placeRate'>Rate:{this.props.place.rate}</span>
       <hr/>
       <div className='placeDetailsContainer'>
         <div className='placeTimeDetails'>
-          <div dangerouslySetInnerHTML={{ __html: hoursStr }} />
+          <div dangerouslySetInnerHTML={{__html: hoursStr}}/>
         </div>
         <a href={this.props.place.menu==='No menu' ? null:this.props.place.menu} target='_blank'><img src='../images/menu-75.jpg' alt='Menu'></img></a>
         <img src={this.props.place.pic} alt={this.props.place.pic}></img>
