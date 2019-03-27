2d91138bfee49f7ad5c900c45e079d120774651a
Kubr <kubr2017@gmail.com>
2019-03-26 23:31:46 -0400

    Feat: Finish design in places cards

--- src/components/Place.js
+++ src/components/Place.js
@@ -10,7 +10,7 @@ class Place extends React.Component {
     console.log('inside place, name:',this.props.place.name,' LoadStatus:',this.props.LoadStatus,' focus:',this.props.focus,' inside place rate:',this.props.place.rate);
     let hoursStr = 'No hours'
     if (!(this.props.place.hours==='No hours')){
-      hoursStr='<h3>Days:</h3> '+this.props.place.hours.days+'<h3>Open hours:</h3><p>'+this.props.place.hours.open[0].renderedTime+'</p>'
+      hoursStr='<p>Days:</p> '+'<span>'+this.props.place.hours.days+'</span>'+'<p>Open hours:</p><span>'+this.props.place.hours.open[0].renderedTime+'</span>'
       console.log('inside place condition No hours name:'+this.props.place.name+'hoursStr:'+hoursStr);
       //<iframe id="frame" src={this.props.place.menu} width='300' height='300' scrolling="yes"></iframe>
     }
@@ -21,17 +21,20 @@ class Place extends React.Component {
             <h2 className='placeName'>{this.props.place.name}</h2>
           </div>
           <div className='placeRateContainer'>
-            <span className='placeRate'>Rate:{this.props.place.rate}</span>
+            <span className='placeRate'>Rate:</span> {this.props.place.rate}
           </div>
         </div>
       <div className='placeDetailsContainer'>
         <img src={this.props.place.pic} alt={this.props.place.pic}></img>
-        <a href={this.props.place.menu==='No menu' ? null:this.props.place.menu} target='_blank'><img src='../images/menu-75.jpg' alt='Menu'></img></a>
-        <div className='placeTimeDetails'>
+        <div className='placeMenuContainer'>
+          <span className='placeMenu'>Menu</span>
+          <a href={this.props.place.menu==='No menu' ? null:this.props.place.menu} target='_blank'><img src='../images/menu-75.jpg' alt='Menu'></img></a>
+        </div>
+        <div className='placeTimeContainer'>
           <div dangerouslySetInnerHTML={{__html: hoursStr}}/>
         </div>
         <div className='placeAddressContainer'>
-          <h3 className='placeAddress'>Address:</h3>
+          <span className='placeAddress'>Address:</span>
           <p>{this.props.place.address}</p>
         </div>
       </div>
