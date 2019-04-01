6009e35bb9ce65cb14dd800a6b049d7c07f484cc
r <kubr2017@gmail.com>
2019-03-31 20:10:21 -0400

    Chore: Add APIkeys.js file

---
+++ src/components/APIkeys.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const FourSquareClient_id='YOUR_API_client_id'
+export const FourSquareClient_secret='YOUR_API_client_secret'
+export const GoogleAPIkey='YOUR_API_key'
